8a231fc08576e7f42756ebf7757e463eea60e065
tarunprajapat5152 <tarunprajapat5152@gmail.com>
2025-08-29 15:53:07 +0530

    order

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-const PORT = process.env.PORT || 3000
-console.log(PORT)
+const PORT = process.env.PORT || 3000;
+console.log(PORT);
 
 // MongoDB Connection
 mongoose
@@ -58,18 +58,29 @@ const Order = mongoose.model("orders", orderSchema);
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
 
 // Routes
-app.get('/', (req, res) => {
-    res.send("Hey")
-})
+app.get("/", (req, res) => {
+  res.send("Hey");
+});
 
 app.get("/orderInfo", async (req, res) => {
+  console.log("call");
   const orderDetails = await Order.find({});
   res.json(orderDetails);
 });
 
+app.get("/orderInfo/:id", async (req, res) => {
+  const orderId = req.params.id;
+  const orderInfo = await Order.find({orderId})
+  console.log(orderInfo)
+  res.json(orderInfo)
+});
+
 app.post("/orderDetails", async (req, res) => {
+  console.log("hryy");
+  console.log(req.body);
   if (
     !req.body ||
     !req.body.orderId ||
@@ -81,7 +92,8 @@ app.post("/orderDetails", async (req, res) => {
     !req.body.currentTime ||
     !req.body.currentDistance ||
     !req.body.currentEstimateTime
-  ) return res.status(400).json({ msg: "all fields are require" });
+  )
+    return res.status(400).json({ msg: "all fields are require" });
 
   const order = await Order.create({
     orderId: req.body.orderId,
@@ -92,10 +104,10 @@ app.post("/orderDetails", async (req, res) => {
     distance: req.body.distance,
     currentTime: req.body.currentTime,
     currentDistance: req.body.currentDistance,
-    currentEstimateTime: req.body.currentEstimateTime
-  })
+    currentEstimateTime: req.body.currentEstimateTime,
+  });
 
-  res.status(201).json({order});
+  res.status(201).json({ order });
 });
 
 app.listen(PORT, () => console.log("sever is runing"));
